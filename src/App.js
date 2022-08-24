e5e633163bf1b920b9e37b86b4ddcface860a4bc
Mike Oram <Mikeoram01@gmail.com>
2022-08-24 19:53:03 +0100

    Added the render function.

--- src/App.js
+++ src/App.js
@@ -40,6 +40,85 @@ class App extends Component {
       return '0' + num;  
   }
 
-  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className='msg'>
+            <div  className='year'>
+            <span>
+            {this.state.year}</span> / Year
+            </div>
+          </div>
+          <div className='M_month'>
+          {`${this.state.M_month}`}
+          </div>
+          <div className='box'>
+
+           
+
+            {this.array(12).map((x,index)=>{
+              return (
+                <div key={index} className={`month item ${index===(this.state.month-1)?"active":""}`} style={{transform: `rotate(${index*30-30*(this.state.month-1)}deg)`}}>
+                  {`${x} month`}
+                </div>
+              )
+            })}
+
+            {this.array(30).map((x,index)=>{
+              return (
+                <div key={index} className={`day item ${index===(this.state.day-1)?"active":""}`} style={{transform: `rotate(${index*12-12*(this.state.day-1)}deg)`}}>
+                  {`${x} day`}
+                </div>
+              )
+            })}
+
+            {this.array(7).map((x,index)=>{
+              return (
+                <div key={index} 
+                className={`week item ${index===(this.state.week-1)?"active":""}`} 
+                style={{transform: `rotate(${index*(360/21)-(360/21)*(this.state.week-1)}deg)`}}>
+                  {`week ${x}`}
+                </div>
+              )
+            })}
+
+            {this.array(24).map((x,index)=>{
+              return (
+                <div key={index} 
+                className={`hour item ${index===(this.state.hour-1)?"active":""}`} 
+                style={{transform: `rotate(${index*(360/24)-(360/24)*(this.state.hour-1)}deg)`}}>
+                  {`${x} hr`}
+                </div>
+              )
+            })}
+
+            {this.array(60).map((x,index)=>{
+              return (
+                <div key={index} 
+                className={`Minute item ${index===(this.state.Minute-1)?"active":""}`} 
+                style={{transform: `rotate(${index*(360/60)-(360/60)*(this.state.Minute-1)}deg)`}}>
+                  {`${x} min`}
+                </div>
+              )
+            })}
+
+            {this.array(60).map((x,index)=>{
+              return (
+                <div key={index} 
+                className={`second item ${index===(this.state.second-1)?"active":""}`} 
+                style={{transform: `rotate(${index*(360/60)-(360/60)*(this.state.second-1)}deg)`}}>
+                  {`${x} sec`}
+                </div>
+              )
+            })}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
