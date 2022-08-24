a3613d7ba9e4dba53a5da79b9c253eab3b7c19ea
Mike Oram <Mikeoram01@gmail.com>
2022-08-24 19:45:03 +0100

    Added data for State.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ class App extends Component {
   constructor(){
     super()
     this.state = ({
-      
+      year: 2022,
+      M_month: 'Aug',
+      month: 1,
+      day: 1, 
+      week: 1, 
+      hour: 12,
+      Minute: 1,
+      second: 1,
+      Zodiac: 'Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces'.split(','),
+      M_months: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Spt","Oct","Nov","Dec"],
+
     })
   }
 }
+
+
 
 export default App;
